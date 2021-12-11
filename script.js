a5ad2316efc678d3919d660a96c4a0a2c3438536
unico72 <90961740+unico72@users.noreply.github.com>
2021-12-11 19:43:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 let win = open("https://market.kz");
 if(win){
             win.addEventListener("DOMContentLoaded", function (evt)
