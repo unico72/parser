1bada66cfa992a2421c49cf6fd616617609d3c42
unico72 <90961740+unico72@users.noreply.github.com>
2021-12-11 19:10:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("123");
+let win = open("http://site.com");
+            
+            win.addEventListener("DOMContentLoaded", function (evt)
+            {
+                console.log(win.document.title);
+                var i = document.querySelectorAll('.grid-ads-item-compact');
+                alert(i[0].textContent)
+            });
