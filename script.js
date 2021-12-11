7dbbfc0b59327defd968048fee29d2181bd8ec20
unico72 <90961740+unico72@users.noreply.github.com>
2021-12-11 19:12:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let win = open("https://market.kz");
             win.addEventListener("DOMContentLoaded", function (evt)
             {
                 console.log(win.document.title);
-                var i = document.querySelectorAll('.grid-ads-item-compact');
+                var i = win.document.querySelectorAll('.grid-ads-item-compact');
                 alert(i[0].textContent)
             });
